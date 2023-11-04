71ff208a186df871d8013bb2222791e72c8b7a0d
sakshi <panchalsakshi01@gmail.com>
2023-11-04 04:50:27 -0700

    deployment error fixed

--- app/review/page.tsx
+++ app/review/page.tsx
@@ -1,8 +1,7 @@
-import ReviewPage from "@/components/reviewPage";
 export default function Review() {
     return (
         <div className="min-h-screen max-h-screen w-[49vw] flex items-center justify-center text-5xl p-5 mr-20 bg-blue-900 font-extrabold items-center pt-2 flex flex-col">
-        <ReviewPage />
+          Hey! It's Dashboard Page.
         </div>
     )
 }
