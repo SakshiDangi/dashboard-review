ed0de5ea9caca5037849ca6a1174a4253bc61a40
sakshi <panchalsakshi01@gmail.com>
2023-11-04 04:53:00 -0700

    deploy fix error

--- app/review/page.tsx
+++ app/review/page.tsx
@@ -1,7 +1,11 @@
-export default function Review() {
-    return (
-        <div className="min-h-screen max-h-screen w-[49vw] flex items-center justify-center text-5xl p-5 mr-20 bg-blue-900 font-extrabold items-center pt-2 flex flex-col">
-          Hey! It's Dashboard Page.
-        </div>
-    )
+import React from 'react'
+
+function page() {
+  return (
+    <div className="min-h-screen max-h-screen w-[49vw] flex items-center justify-center text-5xl p-5 mr-20 bg-blue-900 font-extrabold items-center pt-2 flex flex-col">
+      Hey! It's Dashboard Page     
+    </div>
+  )
 }
+
+export default page
