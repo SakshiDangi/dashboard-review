7ae5c6c37e1b85f795bf663fc804958a42306d27
sakshi <panchalsakshi01@gmail.com>
2023-11-04 05:17:38 -0700

    error fixed

--- app/review/page.tsx
+++ app/review/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function page() {
   return (
     <div className="min-h-screen max-h-screen w-[49vw] flex items-center justify-center p-5 mr-20 bg-blue-900 items-center pt-2 flex flex-col">
-      <h1 className='font-extrabold text-5xl'>Hey! It's Dashboard Page</h1>     
+      <h1 className='font-extrabold text-5xl'>Hey Its Dashboard Page</h1>     
     </div>
   )
 }
